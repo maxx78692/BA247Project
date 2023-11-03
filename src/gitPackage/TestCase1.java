e73a115f4339823799c5af6a6f0aad0ee77e4f8d
HP <HP@DESKTOP-7SUOF5D>
2023-11-03 11:22:35 +0530

    TestCase 1 is Modified and TestCase 2 is new added.

--- src/gitPackage/TestCase1.java
+++ src/gitPackage/TestCase1.java
@@ -3,8 +3,9 @@ package gitPackage;
 public class TestCase1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("This is TestCase1");
+		System.out.println("Modifying Test Case1");
 	}
 
 }
